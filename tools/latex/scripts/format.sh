e317278fff813e208cc45e45d8d5c1dea0e677ad
Tetsuya.dev <256985235+tetsuya-dev-jp@users.noreply.github.com>
2026-04-18 13:32:08 +0900

    fix: avoid latexindent backup artifacts on save

--- tools/latex/scripts/format.sh
+++ tools/latex/scripts/format.sh
@@ -9,5 +9,10 @@ require_file_arg "$@"
 
 target_path="$(normalize_path "$1")"
 target_relative="$(workspace_relative "$target_path")"
+temp_relative="${target_relative}.format-tmp"
 
-docker_run latexindent -w -l=/workspace/tools/latex/.latexindent.yaml "/workspace/$target_relative"
+rm -f "$WORKSPACE_DIR/$temp_relative"
+
+docker_run sh -lc "latexindent -g=/tmp/indent.log -l=/workspace/tools/latex/.latexindent.yaml \"/workspace/$target_relative\" > \"/workspace/$temp_relative\""
+
+mv "$WORKSPACE_DIR/$temp_relative" "$target_path"
